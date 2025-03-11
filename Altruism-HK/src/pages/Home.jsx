493f5d4146dbad4074796731d9940433e364354e
Joseph Lau <lau.joseph.ca@gmail.com>
2025-03-11 16:43:43 +0800

    edit

---
+++ Altruism-HK/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+        <div className="hero">
+            <h1>"From the heart" makes you healthy, comfortable,
+            The truth of flying freely!</h1>
+        </div>
+        <div className="card-section">
+            <div className="brief-card">
+                <div>
+                    <h2>Mindset matters, more than you think.</h2>
+                    <h3>Understanding the importance of mental health is key. That's why we start with a full evaluation to make sure we're touching on important factors in your relationship with food and wellness.</h3>
+                </div>
+                <img src="images/acupuncture.jpg" id="acupuncture-img"></img>
+            </div>
+            <div className="brief-card">
+                <img src="images/massage.jpg" id="massage-img"></img>
+                <div>
+                    <h2>Mindset matters, more than you think.</h2>
+                    <h3>Understanding the importance of mental health is key. That's why we start with a full evaluation to make sure we're touching on important factors in your relationship with food and wellness.</h3>
+                </div>
+            </div>
+            
+        </div>  
+        <div></div>
+        <div></div>
+        </>
+    )
+}
