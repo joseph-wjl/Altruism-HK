89c1d08baa01090d07900402866a8468011d092f
Joseph Lau <lau.joseph.ca@gmail.com>
2025-03-24 21:28:39 +0800

    edit

--- Altruism-HK/src/components/Nav.jsx
+++ Altruism-HK/src/components/Nav.jsx
@@ -3,14 +3,26 @@ import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-light bg-light navbar-expand-lg" >
             <a href="#" className="navbar-brand">Altruism</a>
-            <ul className="navbar-nav">
-                <Link to='pages/Home' className='navbar-link'>Home</Link>
-                <Link to='pages/Team' className='navbar-item'>Team</Link>
-                <Link to='pages/Contact' className='navbar-item'>Contact</Link>
-                <Link to='pages/About' className='navbar-item'>About</Link>
-            </ul>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">Open</button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to='pages/Home' className='navbar-link'>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='pages/Team' className='navbar-link'>Team</Link>
+                    </li>
+                        <li className="nav-item">
+                    <Link to='pages/Contact' className='navbar-link'>Contact</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='pages/About' className='navbar-link'>About</Link>
+                    </li>
+                </ul>
+            </div>
+            
         </nav>
     )
 }
