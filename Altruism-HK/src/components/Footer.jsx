d0ca4e16042bba4f2425898fba89ebe8659ebb3c
Joseph Lau <lau.joseph.ca@gmail.com>
2025-03-27 01:42:23 +0800

    edit

--- Altruism-HK/src/components/Footer.jsx
+++ Altruism-HK/src/components/Footer.jsx
@@ -4,11 +4,13 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer>
-            <h4>Join Altruism</h4>
-            <label for="email-input">Email address</label>
-            <input type="email" placeholder="Enter your email address" id="email-input"></input>
-            <button>Subscribe</button>
-            <div id="social">
+            <div id="email-section">
+                <h4>Join Altruism</h4>
+                <label for="email-input">Email address</label>
+                <input type="email" placeholder="Enter your email address" id="email-input"></input>
+                <button>Subscribe</button>
+            </div>
+            <div id="social-section">
                 <p>Social</p>
                 <p>Instagram</p>
                 <p>Facebook</p>
