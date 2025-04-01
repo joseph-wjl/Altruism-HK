794ee493d1387a36d431f5994dd8897d2d4d6401
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-01 20:24:58 +0800

    edit

--- Altruism-HK/src/components/Nav.jsx
+++ Altruism-HK/src/components/Nav.jsx
@@ -3,24 +3,52 @@ import { Link } from 'react-router-dom'
 import './Nav.css'
 
 export default function Nav() {
+    
+    const [isNavOpen, setIsNavOpen] = React.useState(false);
+
+    function toggleNav() {
+        setIsNavOpen(!isNavOpen)
+    }
+
     return (
-        <div class="container bg-light">
-            <nav class="navbar navbar-light navbar-expand-lg">
-                <a href="#" class="navbar-brand">Altruism</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuItems" aria-controls="menuItems" aria-expanded="false" aria-label="Toggle Navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <div className="container bg-light" >
+            <nav className={`navbar navbar-light navbar-expand-lg ${isNavOpen? 'navbar-white' : ''}`}>
+                <a href="#" className={`navbar-brand ${isNavOpen? 'navbar-brand-black' : ''}`}>Altruism</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#menuItems"
+                    aria-controls="menuItems"
+                    aria-expanded="false"
+                    aria-label="Toggle Navigation"
+                    onClick={toggleNav}
+                >
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="menuItems">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
+                <div className="collapse navbar-collapse" id="menuItems">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
                             <Link to='pages/Home' className='navbar-link'>Home</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
+                            <Link to='pages/About' className='navbar-link'>About</Link>
+                        </li>
+                        <li className="nav-item">
                             <Link to='pages/Team' className='navbar-link'>Team</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
+                            <Link to='pages/Services' className='navbar-link'>Services</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='pages/Fees' className='navbar-link'>Fees</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='pages/Equipment' className='navbar-link'>Equipment</Link>
+                        </li>
+                        <li className="nav-item">
                             <Link to='pages/Contact' className='navbar-link'>Contact</Link>
-                            {/* <a href="#" class="nav-link">Contact</a> */}
+                            {/* <a href="#" className="nav-link">Contact</a> */}
                         </li>
                     </ul>
                 </div>
