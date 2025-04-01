718fb2c8a7ed1f83dd9cd58093f0184b0afe416b
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-02 01:00:53 +0800

    Update Home.jsx

--- Altruism-HK/src/pages/Home.jsx
+++ Altruism-HK/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
+// import Nav from '../components/Nav'
+// import Footer from '../components/Footer'
 
 export default function Home() {
     return (
