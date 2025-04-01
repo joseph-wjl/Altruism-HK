7d96e6b9ceda300e7dfdfe1ce44ece58ca12ab53
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-01 18:26:43 +0800

    Update Nav.jsx

--- Altruism-HK/src/components/Nav.jsx
+++ Altruism-HK/src/components/Nav.jsx
@@ -4,6 +4,29 @@ import './Nav.css'
 
 export default function Nav() {
     return (
+        <div class="container bg-light">
+            <nav class="navbar navbar-light navbar-expand-lg">
+                <a href="#" class="navbar-brand">Altruism</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuItems" aria-controls="menuItems" aria-expanded="false" aria-label="Toggle Navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="menuItems">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <Link to='pages/Home' className='navbar-link'>Home</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to='pages/Team' className='navbar-link'>Team</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to='pages/Contact' className='navbar-link'>Contact</Link>
+                            {/* <a href="#" class="nav-link">Contact</a> */}
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+            
         // <nav className="navbar navbar-light navbar-expand-lg" >
         //     <a href="#" className="navbar-brand">Altruism</a>
         //     <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
@@ -26,30 +49,6 @@ export default function Nav() {
         //         </ul>
         //     </div>
         // </nav>
-
-    <div class="container bg-dark">
-        <nav class="navbar navbar-light navbar-expand-lg">
-            <a href="#" class="navbar-brand">Altruism</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuItems" aria-controls="menuItems" aria-expanded="false" aria-label="Toggle Navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="menuItems">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <Link to='pages/Home' className='navbar-link'>Home</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to='pages/Team' className='navbar-link'>Team</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to='pages/Contact' className='navbar-link'>Contact</Link>
-                        {/* <a href="#" class="nav-link">Contact</a> */}
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-            
         
     )
 }
