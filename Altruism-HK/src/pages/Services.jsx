b40a2cf4dd2b428e7922dd7cfec7c45389d46dab
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-06 17:38:48 +0800

    edit

--- Altruism-HK/src/pages/Services.jsx
+++ Altruism-HK/src/pages/Services.jsx
@@ -57,6 +57,69 @@ export default function Services(){
                         </div> */}
                     
                 </div> 
+                <div className="services-details-container">
+                    <div className="services-details">
+                        <img src="/images/acupuncture.jpg" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Acupuncture</h2>
+                            <p>By stimulating the acupuncture points with fine needles, the purpose of regulating qi and calming the mind, and curing diseases can be achieved. Holistic acupuncture can balance the yin and yang, qi and blood of the five internal organs.</p>
+                            <p>Local acupuncture can help dredge the meridians, promote blood circulation and qi circulation, and achieve the purpose of curing diseases, health care, and health preservation.</p>
+                        </div>
+                    </div>
+                    <div className="services-details">
+                        <img src="/images/cupping.avif" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Cupping</h2>
+                            <p>The negative pressure in cupping is used to make it adsorb onto the skin, causing local congestion, so as to achieve the effects of expelling cold and dampness, removing stagnation, removing toxins and purging heat, promoting qi and blood circulation, unblocking meridians, reducing swelling and relieving pain.</p>
+                            <p>Therefore, it can relieve fatigue, enhance physical fitness, cure and prevent diseases.</p>
+                        </div>
+                    </div>
+                    <div className="services-details">
+                        <img src="/images/injection.jpg" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Beauty injection</h2>
+                            <p>Using needles as thin as hair, based on the theory of TCM meridian points and modern facial anatomy, the points or local facial muscle fascia are micro-stimulated to achieve the effects of lifting, firming, lightening spots, removing acne, removing wrinkles, and nourishing the skin.</p>
+                        </div>
+                    </div>
+                    <div className="services-details">
+                        <img src="/images/moxibustion.avif" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Moxibustion</h2>
+                            <p>By burning moxa cones, moxa wool, moxa sticks, etc. made from moxa leaves, the acupuncture points of the human body can be fumigated and burned to achieve the purpose of warming the meridians and dispelling cold, promoting qi and unblocking collaterals, raising yang and lifting sunken, supporting yang and consolidating deficiency, and maintaining health.</p>
+                        </div>
+                    </div>
+                    <div className="services-details">
+                        <img src="/images/massage.jpeg" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Chinese Massage Therapy</h2>
+                            <p>Under the guidance of traditional Chinese medicine theory, a treatment method that uses special techniques to act on specific parts of the patient's limbs and perform passive exercise.</p>
+                            <p>It has the functions of harmonizing blood and qi, dredging meridians, and promoting metabolism, thereby achieving the effect of curing diseases and maintaining health.</p>
+                        </div>
+                    </div>
+                    <div className="services-details">
+                        <img src="/images/pain-treatment.avif" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Pain Treatment</h2>
+                            <p>In addition to using traditional Chinese medicine acupuncture, orthopedic surgery, and massage techniques to treat pain, our clinic is also equipped with modern advanced treatment equipment, such as electronic shock wave therapy equipment, medium frequency interference current therapy equipment, far infrared electromagnetic wave spectrum equipment, etc., combined with modern anatomical and neurological theories, for comprehensive integrated treatment.</p>
+                        </div>
+                    </div>
+                    <div className="services-details">
+                        <img src="/images/belly-tie.jpg" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Postpartum Rehabilitation (Rehabilitation Belly Tie)</h2>
+                            <p>Pregnancy and childbirth often cause the pelvis to widen, the hips to widen, and the uterus to enlarge, which causes compression and displacement of internal organs, leading to the stretching and separation of the rectus abdominis muscles, resulting in postpartum low back pain and pelvic misalignment.</p>
+                            <p>Through human body structure analysis, manual therapy, and abdominal piercing treatment, we can effectively reshape the body and improve problems such as low back pain, rectus abdominis separation, and pelvic misalignment.</p>
+                        </div>
+                    </div>
+                    <div className="services-details">
+                        <img src="/images/tianjiu.jpg" className="services-details-img"></img>
+                        <div className="services-details-text">
+                            <h2>Tianjiu Therapy</h2>
+                            <p>During the season with the most yang energy (dog days) and the season with the heaviest cold energy (nine days), Chinese medicine practitioners grind pungent and warm herbs into powder, add ginger juice and mix well before applying it to the prescribed acupoints.</p>
+                            <p>Through the stimulation of acupuncture points by drugs, the effects of warming the meridians and dispelling cold can be achieved, the meridians can be unblocked, blood circulation and blood vessels can be activated, the functions of internal organs can be adjusted, the body's yang energy can be mobilized, and the body's immunity can be enhanced.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
