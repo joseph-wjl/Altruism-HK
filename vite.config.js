e90dad4a701457d304c8e78243362c4bd9c5381b
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-07 02:25:33 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 export default {
     build: {
-        outDir: 'dist',
+        outDir: 'dist', // Output directory
         assetsDir: 'assets', // Ensure assets are placed in 'dist/assets'
+        emptyOutDir: true, // Clean the output directory before building
     },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     output: {
-        path: __dirname + '/dist',
+        path: __dirname + '/dist', // Output directory
         filename: 'bundle.js',
         assetModuleFilename: 'assets/[name][ext]', // Ensure assets are placed in 'dist/assets'
+        clean: true, // Clean the output directory before building
     },
     // ...existing configuration...
 };
