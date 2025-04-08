bdb1f29a34ce56fd9f758ca7d3ca2c0057d4c099
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-09 00:35:00 +0800

    edit

--- Altruism-HK/src/pages/Equipment.jsx
+++ Altruism-HK/src/pages/Equipment.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// import Spline from '@splinetool/react-spline';
 
 export default function Equipment(){
     return (
-        <h1>Equipment page</h1>
+        <>
+            <h1>Equipment page</h1>
+            {/* <Spline
+                scene="https://prod.spline.design/3ArfHPRWmgJbL5vN/scene.splinecode" 
+            /> */}
+        </>
     )
 }
