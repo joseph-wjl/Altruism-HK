3f04083e0dced7e9048071010276485b9a21a081
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-10 20:09:41 +0800

    edit

--- Altruism-HK/src/pages/Equipment.jsx
+++ Altruism-HK/src/pages/Equipment.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Equipment.css'
 // import Spline from '@splinetool/react-spline';
 
 export default function Equipment(){
     return (
         <>
             <h1>Equipment page</h1>
+            <div className="equipment-container">
+                <div className="equipment-hero"></div>
+            </div>
+
             {/* <Spline
                 scene="https://prod.spline.design/3ArfHPRWmgJbL5vN/scene.splinecode" 
             /> */}
